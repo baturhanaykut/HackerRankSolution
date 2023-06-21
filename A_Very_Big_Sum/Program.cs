4f5df91b701c433a9ca342de6c8d1ba7d80f1ca0
Baturhan Aykut <baturhanaykut@gmail.com>
2023-06-22 01:16:45 +0300

    Add project files.

---
+++ A_Very_Big_Sum/Program.cs
@@ -0,0 +1,13 @@
+﻿long aVeryBigSum(List<long> ar)
+{
+	long sum =0;
+	foreach (var item in ar)
+	{
+        sum += item;
+	}
+    return sum;
+}
+
+List<long> sayilar = new List<long>() {1,2,3,4,5 };
+
+Console.WriteLine(aVeryBigSum(sayilar));
---
+++ Diagonal_Difference/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+List<int> a = new List<int>() { 1, 2, 3 };
+List<int> b = new List<int>() { 3, 2, 1 };
+
+foreach (var item in compareTriplets(a, b))
+{
+    Console.WriteLine(item);
+}
+
+
+List<int> compareTriplets(List<int> a, List<int> b)
+{
+    List<int> result = new List<int>() { 0, 0 };
+    for (int i = 0; i < 3; i++)
+    {
+        if (a[i] > b[i])
+        {
+            result[0] += 1;
+        }
+        else if (a[i] < b[i])
+        {
+            result[1] += 1;
+        }
+    }
+    return result;
+}
