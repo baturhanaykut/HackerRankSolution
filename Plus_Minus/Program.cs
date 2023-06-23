a9f0acec122a2fa158d68aa08e1f70a333c1727b
Baturhan Aykut <baturhanaykut@gmail.com>
2023-06-24 00:30:43 +0300

    Plus_Minus

---
+++ Plus_Minus/Program.cs
@@ -0,0 +1,34 @@
+﻿void plusMinus(List<int> arr)
+{
+	decimal a = 0;
+	decimal b = 0;
+	decimal c = 0;
+	for (int i = 0; i < arr.Count; i++)
+	{
+	
+		if (arr[i]>0)
+		{
+			a++;
+		}
+		else if (arr[i]==0)
+		{
+			b++;
+		}
+		else
+		{
+			c++;
+		}
+	}
+	a = a / arr.Count;
+	b = b / arr.Count;
+	c = c / arr.Count;
+
+
+	Console.WriteLine(String.Format("{0:0.000000}",a));
+
+    Console.WriteLine(String.Format("{0:0.000000}",b));
+	Console.WriteLine(String.Format("{0:0.000000}",c));
+}
+
+
+plusMinus(new List<int>() { 1,1,0,-1,-1 });
